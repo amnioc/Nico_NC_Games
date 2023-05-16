2f10655e31924bdd0f294d189409fbe049b78739
amnioc <nicdhillon19@gmail.com>
2023-05-16 16:51:50 +0100

    routing for users, reviews and comments

--- routes/reviews-router.js
+++ routes/reviews-router.js
@@ -2,6 +2,8 @@ const {
   getAllReviews,
   getReviewById,
   updateReviewVotes,
+  addReviewComment,
+  getReviewComments,
 } = require("../controllers/reviews.controllers");
 
 const reviewsRouter = require("express").Router();
@@ -9,4 +11,10 @@ const reviewsRouter = require("express").Router();
 reviewsRouter.route("/").get(getAllReviews);
 
 reviewsRouter.route("/:review_id").get(getReviewById).patch(updateReviewVotes);
+
+reviewsRouter
+  .route("/:review_id/comments")
+  .post(addReviewComment)
+  .get(getReviewComments);
+
 module.exports = reviewsRouter;
